acc25f60267eefd4a315ec7dae02caa9b0a84388
Vitor Godeiro <vitorgodeiro@live.com>
2016-06-26 20:20:18 -0300

    Método de Jacobi

---
+++ Jacobi/main.cpp
@@ -0,0 +1,39 @@
+
+/*
+   ==================================================================================
+
+   Arquivo: main.cpp
+   Comentarios: Codigo-fonte principal
+
+   ==================================================================================
+
+   Autor..............: Hiago Mayk Gomes de Araujo Rocha
+                        Vitor de Godeiro Marques
+                        Instito Metropole Digital
+                        Universidade Federal do Rio Grande do Norte
+                        Natal, Rio Grande do Norte, Brasil
+   Data Criação.......: 28/02/2015
+   Ultima Modificação.: 16/03/2016
+   Versao.............: 1.0
+   Para Compilar......:
+   Para execultar.....:
+
+   ==================================================================================
+*/
+
+/* ==================================================================================*/
+// Includes
+
+#include "Jacobi.h"
+
+/* ==================================================================================*/
+//Funcao Principal
+
+int main()
+{
+    //Criacao da classe de Jacobi
+    Jacobi jacobi;
+    jacobi.executarJacobi();
+
+    return 0;
+}
