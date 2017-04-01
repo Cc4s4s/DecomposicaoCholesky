947ca4e295206432888b352f1e211de9009b5cfe
vitorgodeiro <vitorgodeiro@live.com>
2017-04-01 11:31:42 -0300

    Update

---
+++ codigo/FatCholesky.h
@@ -0,0 +1,95 @@
+/*
+   ==================================================================================
+   Arquivo: FatLuParcial.h
+   Comentarios: Declaração da classe responsável pela fatoração de Cholesky.
+   ==================================================================================
+   Autor..............: Hiago Mayk Gomes de Araujo Rocha
+                        Vitor de Godeiro Marques
+                        Instito Metropole Digital
+                        Universidade Federal do Rio Grande do Norte
+                        Natal, Rio Grande do Norte, Brasil
+   Data Criação.......: 28/02/2015
+   Ultima Modificação.: 01/04/2017
+  ==================================================================================
+*/
+
+#ifndef FATCHOLESKY_H
+#define FATCHOLESKY_H
+
+class FatCholesky
+{
+    public:
+
+        /**
+        *   Construtor
+        *   @param void
+        *   @return void
+        */
+        FatCholesky();
+
+        /**
+        *   Destrutor
+        *   @param void
+        *   @return void
+        */
+        virtual ~FatCholesky();
+
+        /**
+        *   Método responsavel por chamar as demais funcoes e
+        *   executar a fatoracao LU com pivotamento parcial
+        *   @param void
+        *   @return void
+        **/
+        void FatExecutar();
+    
+    private:
+         //Funcoes
+
+        /**
+        *   Funcao responsavel por imprimir o cabecalho do menu
+        *   @param void
+        *   @return void
+        **/
+        void menu();
+
+        /**
+        *   Funcao responsavel por pegar a ordem da matriz e cria-la
+        *   @param void
+        *   @return
+        **/
+        int menuDados();
+
+        /**
+        *   Metodo responsavel por prencher a matriz completa do sistema
+        *   @param void
+        *   @return void
+        */
+        void prencheMatriz();
+
+        /**
+        *   Metodo responsavel por impimir na tela o sistema que estamos trabalhando
+        *   @param void
+        *   @return void
+        */
+        void visualizaSistema();
+
+        /**
+        *   Funcao responsavel por fazer a operação de Deconposição de Cholesky
+        *   @param void
+        *   @return void
+        */
+        void fatoraSistema();
+
+        /**
+        *   Metodo responsavel por imprimir a solucao do sistema(X1,X2,...,Xn)
+        *   @param void
+        *   @return void
+        */
+        void solucao();
+
+        //Variaveis
+        int ordem;
+        double **matriz;
+};
+
+#endif // FATCHOLESKY_H
---
+++ codigo/makefile
@@ -0,0 +1,5 @@
+all: exec.exe
+
+exec.exe:
+	 g++ *.cpp -o exe -std=c++11 -O3 -static
+     
